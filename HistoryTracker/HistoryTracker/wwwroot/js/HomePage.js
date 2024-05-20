f1b03f45327f8d41b337c9fae04e3f8f87be41e0
CatalinaVMatei <127317364+CatalinaVMatei@users.noreply.github.com>
2024-05-20 12:56:44 +0300

    Edit java script and cshtml for validation input

--- HistoryTracker/HistoryTracker/wwwroot/js/HomePage.js
+++ HistoryTracker/HistoryTracker/wwwroot/js/HomePage.js
@@ -40,22 +40,24 @@ const getSummaryData = function () {
   
 }
 const getChangeFrequencies = function () {
-    var githubUrl = $('#githubUrl').val();
-    var encodedGithubUrl = encodeURIComponent(githubUrl);
+   
+        var githubUrl = $('#githubUrl').val();
+        var encodedGithubUrl = encodeURIComponent(githubUrl);
 
-    $.ajax({
-        type: "GET",
-        url: "/" + encodedGithubUrl + "/get-change-frequencies",
-        dataType: "json",
-        success: function (response) {
-            $('#changeFrequenciesContainer').html(
-                
-            );
-        },
-        error: function (xhr, status, error) {
-            console.error(xhr.responseText);
-        }
-    });
+        $.ajax({
+            type: "GET",
+            url: "/" + encodedGithubUrl + "/get-change-frequencies",
+            dataType: "json",
+            success: function (response) {
+                $('#changeFrequenciesContainer').html(
+
+                );
+            },
+            error: function (xhr, status, error) {
+                console.error(xhr.responseText);
+            }
+        });
+    
 }
 const redirectToChartPage = function () {
     window.location.href = "/Chart";
@@ -72,28 +74,32 @@ const hideLoader = function() {
     var loader = document.getElementById("loader");
     loader.style.display = "none";
 }
- getComplexityMetrics = function () {
-    var githubUrl = $('#githubUrl').val();
-    var encodedGithubUrl = encodeURIComponent(githubUrl);
+getComplexityMetrics = function () {
+    var isValid = validateAndShowMessage();
+    if (isValid) {
+        var githubUrl = $('#githubUrl').val();
+        var encodedGithubUrl = encodeURIComponent(githubUrl);
 
-    $.ajax({
-        type: "GET",
-        url: "/" + encodedGithubUrl + "/get-complexity-metrics",
-        dataType: "json",
-        beforeSend: function () {
-            showLoader();
-        },
-        success: function (response) {
-            var hotspotsButton = document.getElementById("displayHotspotsFrequencyAndComplexityButton");
-            localStorage.setItem('filePathForHotspotsFrequencyAndComplexityFromAllTime', response);
-            hideLoader();
-            hotspotsButton.style.display = "block";
-        },
-        error: function (xhr, status, error) {
-            console.error(xhr.responseText);
-            hideLoader();
-        }
-    });
+        $.ajax({
+            type: "GET",
+            url: "/" + encodedGithubUrl + "/get-complexity-metrics",
+            dataType: "json",
+            beforeSend: function () {
+                showLoader();
+            },
+
+            success: function (response) {
+                var hotspotsButton = document.getElementById("displayHotspotsFrequencyAndComplexityButton");
+                localStorage.setItem('filePathForHotspotsFrequencyAndComplexityFromAllTime', response);
+                hideLoader();
+                hotspotsButton.style.display = "block";
+            },
+            error: function (xhr, status, error) {
+                console.error(xhr.responseText);
+                hideLoader();
+            }
+        });
+    }
 }
  const getComplexityMetricsForSpecificPeriod = function () {
     var githubUrl = $('#githubUrl').val();
